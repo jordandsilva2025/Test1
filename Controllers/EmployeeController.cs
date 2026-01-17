76842f3b7f03fa26a5e9b404ce50501fb8292349
EDP009 Jordan Dsilva <EDP009@astarserver.local>
2026-01-17 15:14:01 +0530

    Initial commit - MVC2 Project

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC2.Models;
+
+namespace MVC2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee { Id = 1, Name = "John", Department = "IT", Salary = 50000 },
+                new Employee { Id = 2, Name = "Sara", Department = "HR", Salary = 45000 }
+            };
+
+            return View(employees);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MVC2.Models;
+
+namespace MVC2.Data
+{
+    public interface IEmployeeService
+    {
+        List<Employee> GetAll();
+    }
+
+
+}
